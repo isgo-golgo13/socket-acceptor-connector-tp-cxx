4e5db2667298ccf5a84e24b79430312437d3528a
Tomas Wallick <wallick.tom@gmail.com>
2024-09-23 19:39:40 +0200

    updated

--- svckit/socket-connector.cpp
+++ svckit/socket-connector.cpp
@@ -32,6 +32,11 @@ void SocketConnector::connect() {
 //     }
 // }
 
+std::unique_ptr<Socket> SocketConnector::getSocket() {
+    return std::move(socket_);
+}
+
+
 void SocketConnector::sendData(const void* buffer, size_t length) {
     socket_->send(clientSocket_, buffer, length);
 }
